9cb9a00ee0bdb986378c343b9e2e9bfae3b1add7
yahooyahooandrew <124115706+yahooyahooandrew@users.noreply.github.com>
2024-01-21 01:18:29 -0600

    Add files via upload

---
+++ 7 Questions/app.js
@@ -0,0 +1,47 @@
+//using selectors inside the element
+//traversing the dom
+
+const questions = document.querySelectorAll('.question');
+//console.log(questions);
+
+
+questions.forEach(function(question){
+ //   console.log(question);
+    const btn = question.querySelector('.question-btn');
+    //console.log(btn);
+    btn.addEventListener('click',function(){
+        questions.forEach(function(item){
+            //console.log(item);
+            if(item !== question){
+                item.classList.remove("show-text");
+            }
+
+        })
+        question.classList.toggle("show-text");   
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const btns = document.querySelectorAll(".question-btn");
+
+
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function(e) {
+        const question = e.currentTarget.parentElement.parentElement
+        question.classList.toggle("show-text");
+    });
+});
+
+*/
